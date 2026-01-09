132a5283f2ffc6fd56935c3fd5daa30c7d280c9e
Fatima Rostami <frostami.1988@gmail.com>
2026-01-09 23:19:20 +0300

    Add Google Analytics tracking
    - Create GoogleAnalytics component with GA ID: G-Z2FKM0C22E
    - Add Google Analytics to layout for visitor tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { StructuredData } from "@/components/StructuredData";
-import { getAssetPath } from "@/lib/config";
+import { GoogleAnalytics } from "@/components/GoogleAnalytics";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -104,6 +104,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,22 @@
+import Script from 'next/script';
+
+export function GoogleAnalytics() {
+  const GA_ID = 'G-Z2FKM0C22E';
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
